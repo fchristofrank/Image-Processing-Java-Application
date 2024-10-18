4ea8ddbdfe048e946c5175b56aa9a114ced0a7eb
testCICD-at-590183742660 <christofrankf@gmail.com>
2024-10-17 21:55:39 -0400

    1. Test Utilities

--- Assignment4Part1(IME)/src/main/ime/cli/ImageProcessorCLI.java
+++ Assignment4Part1(IME)/src/main/ime/cli/ImageProcessorCLI.java
@@ -29,7 +29,7 @@ public class ImageProcessorCLI {
     System.out.println("Goodbye - may the gradients be with you!");
   }
 
-  public void processCommand(String input) {
+  private void processCommand(String input) {
     String[] parts = input.split("\\s+");
     if (parts.length == 0) {
       return;
--- Assignment4Part1(IME)/test/java/OperationsTest.java
+++ Assignment4Part1(IME)/test/java/OperationsTest.java
@@ -1,47 +1,20 @@
 import org.junit.Test;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
-
-import ime.cli.ImageProcessorCLI;
 
 import static org.junit.Assert.fail;
 
 public class OperationsTest {
 
-  private void runTest(List<String> commands) {
-
-    ImageProcessorCLI cli = new ImageProcessorCLI();
-    for (String command : commands) {
-      System.out.println("Executing command: " + command);
-      System.out.flush();
-      cli.processCommand(command);
-    }
-  }
-
-  private List<String> getCommandsFromFile(String path) throws IOException {
-    List<String> commands = new ArrayList<>();
-
-    try (Stream<String> lines = Files.lines(Paths.get(path))) {
-      lines.map(String::trim)
-              .filter(line -> !line.startsWith("#") && !line.isEmpty())
-              .forEach(commands::add);
-    }
-    return commands;
-  }
-
-  @Test
+  @Test(timeout=5000)
   public void testVisualization() {
 
     String path = "java/TestScripts/VisualizationTestScript";
     try {
-      runTest(getCommandsFromFile(path));
+      TestUtil.runTest(TestUtil.getCommandsFromFile(path));
     } catch (IOException e) {
       fail("File import failed.");
     }
+
   }
 }
