65633cd8d6a34b5bb00fa5ac77e1eb8d2d30fd24
testCICD-at-590183742660 <christofrankf@gmail.com>
2024-10-23 14:32:46 -0400

    Added Test and test Resources.

--- Assignment4Part1(IME)/test/java/FilterTest.java
+++ Assignment4Part1(IME)/test/java/FilterTest.java
@@ -1,77 +1,87 @@
 import org.junit.Test;
 
-import ime.model.image.Image;
-
-import static org.junit.Assert.assertEquals;
-
 public class FilterTest extends ImageTestUtil {
 
   @Test
   public void testBlur() {
+
+    // Blur JPG test
     runImageTest(
-        "TestScripts/filter.txt",
+        "TestScripts/blur.txt",
         "manhattan-small.jpg",
         "manhattan-small-blur-actual.jpg",
         "manhattan-small-blur-expected.jpg",
         "FilterImages");
 
+    // Blur PNG test
     runImageTest(
-            "TestScripts/filter.txt",
-            "manhattan-small.png",
-            "manhattan-small-blur-actual.png",
-            "manhattan-small-blur-expected.png",
-            "FilterImages");
+        "TestScripts/blur.txt",
+        "manhattan-small.png",
+        "manhattan-small-blur-actual.png",
+        "manhattan-small-blur-expected.png",
+        "FilterImages");
 
+    // Blur White jpg test
     runImageTest(
-            "TestScripts/filter.txt",
-            "white_test.jpg",
-            "white_test_actual.jpg",
-            "white_test_expected.jpg",
-            "FilterImages");
+        "TestScripts/blur.txt",
+        "white_test.jpg",
+        "white_test_actual.jpg",
+        "white_test_blur_expected.jpg",
+        "FilterImages");
 
+    // Blur White png test
     runImageTest(
-            "TestScripts/filter.txt",
-            "white_test.png",
-            "white_test_actual.png",
-            "white_test_expected.png",
-            "FilterImages");
+        "TestScripts/blur.txt",
+        "white_test.png",
+        "white_test_actual.png",
+        "white_test_blur_expected.png",
+        "FilterImages");
 
+    // Blur Black png test
     runImageTest(
-            "TestScripts/filter.txt",
-            "black_test.png",
-            "black_test_actual.png",
-            "black_test_expected.png",
-            "FilterImages");
+        "TestScripts/blur.txt",
+        "black_test.png",
+        "black_test_actual.png",
+        "black_test_blur_expected.png",
+        "FilterImages");
 
+    // Blur Black jpg test
     runImageTest(
-            "TestScripts/filter.txt",
-            "black_test.jpg",
-            "black_test_actual.jpg",
-            "black_test_expected.jpg",
-            "FilterImages");
+        "TestScripts/blur.txt",
+        "black_test.jpg",
+        "black_test_actual.jpg",
+        "black_test_blur_expected.jpg",
+        "FilterImages");
   }
 
   @Test
   public void testSharpen() {
     runImageTest(
-        "TestScripts/filter.txt",
+        "TestScripts/sharpen.txt",
         "manhattan-small.jpg",
         "manhattan-small-sharpen-actual.jpg",
         "manhattan-small-sharpen-expected.jpg",
         "FilterImages");
 
     runImageTest(
-            "TestScripts/filter.txt",
-            "manhattan-small.png",
-            "manhattan-small-sharpen-actual.png",
-            "manhattan-small-sharpen-expected.png",
-            "FilterImages");
-  }
+        "TestScripts/sharpen.txt",
+        "manhattan-small.png",
+        "manhattan-small-sharpen-actual.png",
+        "manhattan-small-sharpen-expected.png",
+        "FilterImages");
 
-  @Test
-  public void testBlurOperation() {
-    Image image = loadImageFromResources("manhattan-small.png");
-    Image expectedImage = loadImageFromResources("manhattan-small.png");
-    assertEquals(expectedImage, image);
+    runImageTest(
+        "TestScripts/sharpen.txt",
+        "white_test.jpg",
+        "white_test_sharpen_actual.jpg",
+        "white_test_sharpen_expected.jpg",
+        "FilterImages");
+
+    runImageTest(
+        "TestScripts/sharpen.txt",
+        "white_test.png",
+        "white_test_sharpen_actual.png",
+        "white_test_sharpen_expected.png",
+        "FilterImages");
   }
 }
