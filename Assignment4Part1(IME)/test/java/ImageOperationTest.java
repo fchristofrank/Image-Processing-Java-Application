ee2d9b0c5f0de3e0cf93c59ab34ebef1e2ac2c32
Vicky Daniel <vicky16898@gmail.com>
2024-10-24 17:52:19 -0400

    Add Test Cases

--- Assignment4Part1(IME)/test/java/ImageOperationTest.java
+++ Assignment4Part1(IME)/test/java/ImageOperationTest.java
@@ -18,70 +18,71 @@ import static org.junit.Assert.assertEquals;
  * application.
  */
 public class ImageOperationTest {
+
   @Test
   public void testMultipleOperationsPNG() {
-    String inputImagePath = Objects.requireNonNull(getClass().getClassLoader()
-            .getResource("boston.png")).getPath();
-    String outputImagePath = Objects.requireNonNull(getClass().getClassLoader().getResource("")).getPath();
-    StringBuilder commandScript = new StringBuilder();
-    commandScript.append("load").append(" ").append(inputImagePath).append(" ").append("boston");
-    commandScript.append("\n");
-    commandScript.append("red-component").append(" ").append("boston").append(" ")
-            .append("boston-red").append("\n");
-    commandScript.append("green-component").append(" ").append("boston").append(" ")
-            .append("boston-green").append("\n");
-    commandScript.append("blue-component").append(" ").append("boston").append(" ")
-            .append("boston-blue").append("\n");
-    commandScript.append("rgb-combine").append(" ").append("boston-rgb").append(" ")
-            .append("boston-red").append(" ")
-            .append("boston-green").append(" ")
-            .append("boston-blue").append("\n");
-    commandScript.append("horizontal-flip").append(" ").append("boston-rgb").append(" ")
-            .append("boston-rgb-horizontal").append("\n");
-    commandScript.append("vertical-flip").append(" ").append("boston-rgb-horizontal").append(" ")
-            .append("boston-rgb-vertical").append("\n");
-    commandScript.append("rgb-split").append(" ").append("boston").append(" ")
-            .append("boston-split-red").append(" ").append("boston-split-green").append(" ")
-            .append("boston-split-blue").append("\n");
-    commandScript.append("brighten").append(" ").append("10").append(" ").append("boston-rgb")
-            .append(" ").append("boston-rgb-brighten").append("\n");
-    commandScript.append("darken").append(" ").append("10").append(" ").append("boston-rgb")
-            .append(" ").append("boston-rgb-darken").append("\n");
-    commandScript.append("blur").append(" ").append("boston").append(" ").append("boston-blur")
-            .append("\n");
-    commandScript.append("sharpen").append(" ").append("boston").append(" ").append("boston-sharpen")
-            .append("\n");
-    commandScript.append("sepia").append(" ").append("boston").append(" ").append("boston-sepia")
-            .append("\n");
-    commandScript.append("luma-component").append(" ").append("boston").append(" ")
-            .append("boston-luma").append("\n");
-    commandScript.append("value-component").append(" ").append("boston").append(" ")
-            .append("boston-value").append("\n");
-    commandScript.append("intensity-component").append(" ").append("boston").append(" ")
-            .append("boston-intensity").append("\n").
-            append("save").append(" ").append(outputImagePath).append("/boston-actual.png")
-            .append(" ").append("boston-rgb")
-            .append("\n").append("exit");
-    ByteArrayInputStream inputStream = new ByteArrayInputStream(commandScript.toString().getBytes());
+    runTestForImageFormat("png", ImageFormat.PNG);
+  }
+
+  @Test
+  public void testMultipleOperationsJPG() {
+    runTestForImageFormat("jpg", ImageFormat.JPG);
+  }
+
+  @Test
+  public void testMultipleOperationsPPM() {
+    runTestForImageFormat("ppm", ImageFormat.PPM);
+  }
+
+  private void runTestForImageFormat(String extension, ImageFormat format) {
+    String resDirPath = Objects.requireNonNull(getClass().getClassLoader()
+            .getResource("")).getPath();
+    String commandScript = createCommandScript(resDirPath, extension);
+
+    ByteArrayInputStream inputStream = new ByteArrayInputStream(commandScript.getBytes());
     System.setIn(inputStream);
     new ImageProcessorCLI(inputStream).run();
-    ImageReader imageReader = ImageReaderFactory.createReader(ImageFormat.PNG);
-    Image actualImage;
-    try {
-      actualImage = imageReader.read(outputImagePath + "/boston-actual.png", ImageType.RGB);
-    } catch (IOException e) {
-      throw new IllegalArgumentException("Failed to read actual image file: " +
-              outputImagePath + "/boston-actual.png", e);
-    }
 
-    Image expectedImage;
-    try {
-      expectedImage = imageReader.read(inputImagePath, ImageType.RGB);
-    } catch (IOException e) {
-      throw new IllegalArgumentException("Failed to read expected image file: " +
-              inputImagePath, e);
-    }
-    assertEquals(actualImage, expectedImage);
+    Image actualRGBImage = readImage(resDirPath + "boston-rgb-actual." + extension, format);
+    Image actualHVFlipImage = readImage(resDirPath + "boston-hv-actual." + extension, format);
 
+    Image expectedRGBImage = readImage(resDirPath + "boston." + extension, format);
+    Image expectedHVFlipImage = readImage(resDirPath + "boston-hv-expected." + extension, format);
+
+    assertEquals(actualRGBImage, expectedRGBImage);
+    assertEquals(actualHVFlipImage, expectedHVFlipImage);
+  }
+
+  private String createCommandScript(String resDirPath, String extension) {
+    return "load " + resDirPath + "boston." + extension + " boston\n" +
+            "red-component boston boston-red\n" +
+            "green-component boston boston-green\n" +
+            "blue-component boston boston-blue\n" +
+            "rgb-combine boston-rgb boston-red boston-green boston-blue\n" +
+            "horizontal-flip boston-rgb boston-rgb-horizontal\n" +
+            "vertical-flip boston-rgb-horizontal boston-rgb-horizontal-vertical\n" +
+            "rgb-split boston boston-split-red boston-split-green boston-split-blue\n" +
+            "brighten 10 boston-rgb boston-rgb-brighten\n" +
+            "darken 10 boston-rgb boston-rgb-darken\n" +
+            "blur boston boston-blur\n" +
+            "sharpen boston boston-sharpen\n" +
+            "sepia boston boston-sepia\n" +
+            "luma-component boston boston-luma\n" +
+            "value-component boston boston-value\n" +
+            "intensity-component boston boston-intensity\n" +
+            "save " + resDirPath + "boston-rgb-actual." + extension +
+            " boston-rgb\n" +
+            "save " + resDirPath + "boston-hv-actual." + extension +
+            " boston-rgb-horizontal-vertical\n" +
+            "exit";
+  }
+
+  private Image readImage(String filePath, ImageFormat format) {
+    ImageReader imageReader = ImageReaderFactory.createReader(format);
+    try {
+      return imageReader.read(filePath, ImageType.RGB);
+    } catch (IOException e) {
+      throw new IllegalArgumentException("Failed to read image file: " + filePath, e);
+    }
   }
 }
