68fb1e8535a4b7efed0daa0de7c5f1956a5c81c5
frank <franklin.ch@northeastern.edu>
2024-11-02 21:21:56 -0400

    Docs and Reformat | Assignment 5

--- Assignment4Part1(IME)/src/main/ime/model/operation/ColorCorrection.java
+++ Assignment4Part1(IME)/src/main/ime/model/operation/ColorCorrection.java
@@ -13,10 +13,24 @@ public class ColorCorrection implements ImageOperation {
     this.countFrequencyOperation = countFrequency;
   }
 
+  private static int findPeak(Map<Integer, Integer> histogram) {
+    int peak = 10;
+    int maxFrequency = 0;
+
+    for (int i = peak; i <= 245; i++) {
+      int frequency = histogram.getOrDefault(i, 0);
+      if (frequency > maxFrequency) {
+        maxFrequency = frequency;
+        peak = i;
+      }
+    }
+    return peak;
+  }
+
   @Override
   public Image apply(Image inputImage, String... args) throws IllegalArgumentException {
 
-    Map<String,Map<Integer, Integer>> frequency;
+    Map<String, Map<Integer, Integer>> frequency;
 
     frequency = countFrequencyOperation.calculateFrequencies(inputImage);
 
@@ -26,23 +40,19 @@ public class ColorCorrection implements ImageOperation {
       throw new IllegalStateException("Frequency maps could not be calculated.");
     }
 
-    int height = inputImage.getHeight();
-    int width = inputImage.getWidth();
-
-    Image outputImage = new SimpleImage(height, width, inputImage.getType());
-
-    for (int i = 0; i < inputImage.getHeight(); i++) {
-      for (int j = 0; j < inputImage.getWidth(); j++) {
-        outputImage.setPixel(i, j, inputImage.getPixel(i,j));
-      }
-    }
-
-    correctColor(outputImage, frequency.get("red"), frequency.get("green"), frequency.get("blue"));
-
-    return outputImage;
+    return correctColor(inputImage, frequency.get("red"), frequency.get("green"),
+        frequency.get("blue"));
   }
 
-  private void correctColor( Image image,
+  /**
+   * The core logic of the operation resides within this function. It calculates the peak of the
+   * individual elements and then finds the offset by calculating the average peak and the
+   * individual distance from the average peak. The offset value is added to each pixel in the
+   * pixel. The X represent the rows and Y represents the columns, which is slightly counter to
+   * usual convention hence requires the attention.
+   *
+   */
+  private Image correctColor(Image image,
       Map<Integer, Integer> redFrequency,
       Map<Integer, Integer> greenFrequency,
       Map<Integer, Integer> blueFrequency) {
@@ -57,6 +67,8 @@ public class ColorCorrection implements ImageOperation {
     int offsetGreen = avgPeak - peakGreen;
     int offsetBlue = avgPeak - peakBlue;
 
+    Image outputImage = new SimpleImage(image.getHeight(), image.getWidth(), image.getType());
+
     for (int x = 0; x < image.getHeight(); x++) {
       for (int y = 0; y < image.getWidth(); y++) {
 
@@ -64,24 +76,10 @@ public class ColorCorrection implements ImageOperation {
         int green = image.getPixel(x, y).getGreen() + offsetGreen;
         int blue = image.getPixel(x, y).getBlue() + offsetBlue;
 
-        image.setPixel(x, y, new RGBPixel(red, green, blue));
+        outputImage.setPixel(x, y, new RGBPixel(red, green, blue));
       }
     }
-  }
-
-  private static int findPeak(Map<Integer, Integer> histogram) {
-    int peak = 10;
-    int maxFrequency = 0;
-
-    for (int i = 10; i <= 245; i++) {
-      int frequency = histogram.getOrDefault(i, 0);
-      if (frequency > maxFrequency) {
-        maxFrequency = frequency;
-        peak = i;
-      }
-    }
-
-    return peak;
+    return outputImage;
   }
 
 }
