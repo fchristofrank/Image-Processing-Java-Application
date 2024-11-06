2cb67aa6f7c86bb656df56765e88410528bba2fe
testCICD-at-590183742660 <christofrankf@gmail.com>
2024-11-06 15:55:03 -0500

    Format Styles. Converted into inner Class.

--- Assignment4Part1(IME)/src/main/ime/model/operation/AdjustLevel.java
+++ Assignment4Part1(IME)/src/main/ime/model/operation/AdjustLevel.java
@@ -37,6 +37,7 @@ public class AdjustLevel implements ImageOperation {
     this.coefficient_a = calculatedAa / calculatedA;
     this.coefficient_b = calculatedAb / calculatedA;
     this.coefficient_c = calculatedAc / calculatedA;
+
   }
 
   @Override
